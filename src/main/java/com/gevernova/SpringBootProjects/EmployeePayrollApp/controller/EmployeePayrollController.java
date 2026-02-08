1c5899c49c8ab7fc1e4729fd7df067d57a5e5a24
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-08 12:27:30 +0530

    [saptadipa77]: Added employee controller for EmployeePayRoll

---
+++ src/main/java/com/gevernova/SpringBootProjects/EmployeePayrollApp/controller/EmployeePayrollController.java
@@ -0,0 +1,47 @@
+package com.gevernova.SpringBootProjects.EmployeePayrollApp.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/employeepayrollservice")
+public class EmployeePayrollController {
+
+    private final IEmployeePayrollService service;
+
+    public EmployeePayrollController(IEmployeePayrollService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/create")
+    public EmployeePayrollData create(@Valid @RequestBody EmployeePayrollDTO dto) {
+        return service.create(dto);
+    }
+
+    @GetMapping("/")
+    public List<EmployeePayrollData> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public EmployeePayrollData getById(@PathVariable long id) {
+        return service.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public EmployeePayrollData update(
+            @PathVariable long id,
+            @Valid @RequestBody EmployeePayrollDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable long id) {
+        service.delete(id);
+    }
+
+    @GetMapping("/department/{dept}")
+    public List<EmployeePayrollData> getByDept(@PathVariable String dept) {
+        return service.getByDepartment(dept);
+    }
+}
