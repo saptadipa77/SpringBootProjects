4e16acf9495e2fb42d48204ac53a6b450a738fd2
saptadipa77 <saptadipadatta5@gmail.com>
2026-02-08 12:03:10 +0530

    [saptadipa77]: Added initial setup

---
+++ src/main/java/com/gevernova/SpringBootProjects/SpringBootProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.gevernova.SpringBootProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gevernova/SpringBootProjects/SpringBootProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gevernova.SpringBootProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
